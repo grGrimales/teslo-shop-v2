1c99dca8b63d001b4445fac9fb28469c190cb6af
grGrimales <grojasgrimales@gmail.com>
2024-05-19 15:15:34 -0500

    fix vercel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+
+const next = require('next');
+const { join } = require('path');
+
+// Determina si la aplicación está en modo desarrollo
+const isDev = process.env.NODE_ENV !== 'production';
+
+// Carga la configuración de Next.js desde el archivo de configuración
+const nextConfig = require('./next.config.js');
+
+// Crea una instancia del servidor de Next.js con la configuración extendida
+const nextjsServer = next({
+  dev: isDev,
+  conf: nextConfig // Utiliza directamente la configuración importada
+});
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -10,3 +29,7 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+
+
+
