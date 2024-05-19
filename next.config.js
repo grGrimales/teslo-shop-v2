08ba9dc03a30d0893244b664a5506dd56008622c
grGrimales <grojasgrimales@gmail.com>
2024-05-19 15:36:16 -0500

    fix vercel

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-
-        remotePatterns: [
-            { protocol: 'https', hostname: 'res.cloudinary.com' },
-
-            // https://vercel.com/grgrimales-projects
-            { protocol: 'https', hostname: 'assets.vercel.com' },
-
-            // GET https://teslo-shop-next-nu.vercel.app/_next/image?url=%2Fproducts%2F1633802-00-A_0_2000.jpg&w=640&q=75 400 (Bad Request)
-            { protocol: 'https', hostname: 'teslo-shop-next-nu.vercel.app' }
-
-        ]
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com'
+        },
+        // https://teslo-shop-next-nu.vercel.app/
+        {
+            protocol: 'https',
+            hostname: 'teslo-shop-next-nu.vercel.app'
+        }
+      ]
     }
-};
-
-module.exports = nextConfig;
+  }
+  
+  module.exports = nextConfig
