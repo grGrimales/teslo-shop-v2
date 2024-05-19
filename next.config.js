3b04350c5063a5385dcf666ba6a689a8846ce3b1
grGrimales <grojasgrimales@gmail.com>
2024-05-19 13:54:56 -0500

    fix vercel

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: 'res.cloudinary.com' }
+        ]
+    }
+};
+
+module.exports = nextConfig;
