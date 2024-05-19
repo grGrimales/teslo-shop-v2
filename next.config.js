7517a7e4e6d1d0ddfe63cb5006f9ee612633cf32
grGrimales <grojasgrimales@gmail.com>
2024-05-19 15:31:33 -0500

    fix vercel

--- next.config.js
+++ next.config.js
@@ -7,7 +7,11 @@ const nextConfig = {
             { protocol: 'https', hostname: 'res.cloudinary.com' },
 
             // https://vercel.com/grgrimales-projects
-            { protocol: 'https', hostname: 'assets.vercel.com' }
+            { protocol: 'https', hostname: 'assets.vercel.com' },
+
+            // GET https://teslo-shop-next-nu.vercel.app/_next/image?url=%2Fproducts%2F1633802-00-A_0_2000.jpg&w=640&q=75 400 (Bad Request)
+            { protocol: 'https', hostname: 'teslo-shop-next-nu.vercel.app' }
+
         ]
     }
 };
